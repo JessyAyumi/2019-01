3966265656f8c9fd48cfb14dceabd5f992ad90cd
Jéssica Ayumi Uehara <jessicayumiget@gmail.com>
2019-06-27 17:12:48 -0300

    Atualizando classe para que valide o pais nas funcoes

--- lesson-25/back-end/src/main/java/br/edu/utfpr/servico/ServicoCliente.java
+++ lesson-25/back-end/src/main/java/br/edu/utfpr/servico/ServicoCliente.java
@@ -7,6 +7,7 @@ package br.edu.utfpr.servico;
 
 import java.util.List;
 import br.edu.utfpr.dto.ClienteDTO;
+import br.edu.utfpr.dto.PaisDTO;
 import br.edu.utfpr.excecao.NomeClienteMenor5CaracteresException;
 import org.springframework.http.ResponseEntity;
 import java.util.Optional;
@@ -24,11 +25,16 @@ import org.springframework.web.bind.annotation.*;
 public class ServicoCliente {
 
     private List<ClienteDTO> clientes;
+    private List<PaisDTO> paises;
 
     public ServicoCliente() {
+        paises = Stream.of(
+                PaisDTO.builder().id(1).nome("Brasil").sigla("BR").codigoTelefone(55).build(),
+                PaisDTO.builder().id(2).nome("Japão").sigla("JP").codigoTelefone(81).build()
+        ).collect(Collectors.toList());
         clientes = Stream.of(
-                ClienteDTO.builder().id(1).nome("Teste").idade(20).telefone("111111111").limiteCredito(1111.11).build(),
-                ClienteDTO.builder().id(1).nome("Teste2").idade(22).telefone("222222222").limiteCredito(2222.22).build()
+                ClienteDTO.builder().id(1).nome("Teste").idade(20).telefone("111111111").limiteCredito(1111.11).pais(paises.get(1)).build(),
+                ClienteDTO.builder().id(2).nome("Teste2").idade(22).telefone("222222222").limiteCredito(2222.22).pais(paises.get(1)).build()
         ).collect(Collectors.toList());
     }
 
@@ -39,7 +45,10 @@ public class ServicoCliente {
 
     @PostMapping("/servico/cliente")
     public ResponseEntity<ClienteDTO> criar(@RequestBody ClienteDTO cliente) {
+        Optional<PaisDTO> pais = paises.stream().filter(p -> p.getId() == cliente.getPais().getId()).findAny();
+        
         cliente.setId(clientes.size() + 1);
+        cliente.setPais(pais.get());
         clientes.add(cliente);
         return ResponseEntity.status(201).body(cliente);
     }
@@ -47,7 +56,8 @@ public class ServicoCliente {
     @PutMapping("/servico/cliente/{id}")
     public ResponseEntity<ClienteDTO> alterar(@PathVariable int id, @RequestBody ClienteDTO cliente) {
         Optional<ClienteDTO> clienteExistente = clientes.stream().filter(c -> c.getId() == id).findAny();
-
+        Optional<PaisDTO> pais = paises.stream().filter( p -> p.getId() == cliente.getPais().getId() ).findAny();
+        
         clienteExistente.ifPresent(c -> {
             try {
                 c.setNome(cliente.getNome());
@@ -57,6 +67,7 @@ public class ServicoCliente {
             c.setIdade(cliente.getIdade());
             c.setTelefone(cliente.getTelefone());
             c.setLimiteCredito(cliente.getLimiteCredito());
+            c.setPais(pais.get());
         });
 
         return ResponseEntity.of(clienteExistente);
